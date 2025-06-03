1f545ff2b9f1d447181d5e4a70cb14f3ad262d13
tutaf <andrei.cernisov@isa.utm.md>
2025-06-03 08:36:08 +0300

    fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  server: {
+    mimeTypes: {
+      'application/javascript': ['js']
+    }
+  }
 })
